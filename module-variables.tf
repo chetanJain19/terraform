3c6016fff32eb2f60a9843994fd9af7e7c4acb10
chetanJain19 <146300962+chetanJain19@users.noreply.github.com>
2023-12-20 16:14:56 +0530

    Update module-variables.tf

--- module-variables.tf
+++ module-variables.tf
@@ -1,19 +1,45 @@
+#variable "ami_id" {
+#    default = "ami-041d6256ed0f2061c"  #AMI ID specific to Mumbai Region
+#}
+
+#variable "instance_type" {
+#    default = "t2.micro"
+#}
+
+#variable "vpc_id" {
+#    default = "vpc-799e4c12"    #VPC ID of my account's default vpc
+#}
+
+#variable "port" {
+#    default = 22
+#}
+
+#variable "cidr_block" {
+#    default = "0.0.0.0/0"
+#}
+ubuntu@ip-172-31-33-182:~/module$ cat variable.tf
 variable "ami_id" {
-    default = "ami-041d6256ed0f2061c"  #AMI ID specific to Mumbai Region
+  type    = string
+  default = "ami-03f4878755434977f" #AMI ID specific to Mumbai Region
 }
 
 variable "instance_type" {
-    default = "t2.micro"
+  type    = string
+  default = "t2.micro"
 }
 
 variable "vpc_id" {
-    default = "vpc-799e4c12"    #VPC ID of my account's default vpc
+  type    = string
+  default = "vpc-07784f8276b28fb59" #VPC ID of my account's default vpc
 }
 
 variable "port" {
-    default = 22
+  type    = number
+  default = 22
 }
 
 variable "cidr_block" {
-    default = "0.0.0.0/0"
+  type    = string
+  default = "0.0.0.0/0"
 }
+
